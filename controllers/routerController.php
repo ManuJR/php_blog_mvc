24186f39325571702b4497f093af34fbc65ff08e
ManuJR <manu.jr9@gmail.com>
2021-07-13 11:55:26 +0200

    Refactor de la gestión de rutas con un único método que se encarga de comprobar uris que deberían llegar con la que llega y envía a controlador y la acción del controlador adecuada.

--- controllers/routerController.php
+++ controllers/routerController.php
@@ -18,76 +18,77 @@
 
         }
 
+        public function request( $method, $uri, $controller, $action ){
+            $id = null;
+
+            /* Dos tipos de url:
+                1.  /article/create, /login, /signup ...
+                2.  /article/3 , /article/edit/34  ...
+            */
+
+            // Sustituimos el caracter / por la / escapada: \/ , para generar la expresión regular de forma dinámica con cualquier uri.
+            $regexp_uri = str_replace('/', '\/', $uri);
+
+            // Utilizaremos :id para los parámetros /id en las uris. Por eso, sustituimos el string :id, si viene, por [0-9]+, para ponerlo en la exp. regular
+
+            // Si estamos en una uri que acaba en :id y la url que nos llega acaba en número, estamos en una url con parámetro /id
+        
+
+            //if( preg_match("/:id$/", $uri) && preg_match("/[0-9]+$/", $this->uri) ){
+                $regexp_uri = str_replace(':id', '[0-9]+', $regexp_uri);
+                $base_url = preg_replace("/[0-9]+$/", "" ,$this->uri);
+                $id = str_replace( $base_url, "",  $this->uri );
+           // }
+
+
+            // Todas las exp. regulares empiezan y acaban por lo mismo /^uri_que_viene$/
+            $regExp = "/^".$regexp_uri."$/";
+
+            $validate_uri = preg_match($regExp, $this->uri);
+           
+   
+            if( $this->method == $method && $validate_uri){
+               
+                $controller = new $controller();
+                $controller -> $action( $id );
+            }
+        }
+
         public function manageUris(){
            
             
-            if( $this->method == "GET" && $this->uri == "/" ){
+            $this->request( "GET", "/", "WebController", "index");
+            $this->request( "GET", "/login", "WebController", "login");
+            $this->request( "GET", "/signup", "WebController", "signup");
 
-                $webController = new WebController();
-                $webController->index();
-                
-            }
-
-            if( $this->method == "GET" && $this->uri == "/login" ){
-
-                $webController = new WebController();
-                $webController->login();
-                
-            }
-
-            if( $this->method == "GET" && $this->uri == "/signup" ){
-                
-                $webController = new WebController();
-                $webController->signup();
-                
-            }
-
-
-            /*  
-            Rutas de USUARIO:
-                GET     /user/{id}          VIEW USER
-                POST    /user/{id}          UPDATE
-                GET     /user/edit/{id}     VIEW EDIT USER
-                POST    /user/delete/{id}   BORRAR USUARIO
-                POST    /user/signup        CREAR USARIO
-
-            */
             // POST    /signup        CREAR USARIO
-            if( $this->method == "POST" && $this->uri == "/signup" ){
-                
-                $userController = new UserController();
-                $userController->create();
-                
-            }
-            // POST    /login        Login USARIO
-            if( $this->method == "POST" && $this->uri == "/login" ){
-                
-                $userController = new UserController();
-                $userController->login();
-                
-            }
+            $this->request( "POST", "/signup", "UserController", "create");
 
+            // POST    /login        Login USARIO
+            $this->request( "POST", "/login", "UserController", "login");
+            
             // POST    /logout        Logout USARIO
-            if( $this->method == "POST" && $this->uri == "/logout" ){
-                
-                $userController = new UserController();
-                $userController->logout();
-                
-            }
+            $this->request( "POST", "/logout", "UserController", "logout");
 
             // Vista de creación de artículo
-            if( $this->method == "GET" && $this->uri == "/article/new" ){
-            
-                $articleController = new ArticleController();
-                $articleController -> new();
-            }
+            $this->request( "GET", "/article/new", "ArticleController", "new");
 
             // Creación de artículo
-            if( $this->method == "POST" && $this->uri == "/article/create" ){
+            $this->request( "POST", "/article/create", "ArticleController", "create");
+
             
-                $articleController = new ArticleController();
-                $articleController -> create();
-            }
+
+            // Visualización de artículo /article/{id}
+            //$this->request( "POST", "/article/:id", "ArticleController", "show");
+
+
+            // if( $this->method == "GET" && preg_match("/^\/article\/[0-9]+$/", $this->uri) ){
+            //     $id = str_replace( "/article/", "",  $this->uri );
+            //     $articleController = new ArticleController();
+            //     $articleController -> show( $id );
+            // }
+
+            $this->request( "GET", "/article/:id", "ArticleController", "show");
         } 
     
     }
